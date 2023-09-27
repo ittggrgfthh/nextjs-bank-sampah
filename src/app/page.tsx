51595454fe5e30bb5b72d06d54f6f8c9f175402e
lyrihkaesa <kaesalyrih@gmail.com>
2023-09-27 18:12:29 +0700

    change favicon title and desc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
-import Script from "next/script";
+import Image from 'next/image';
+import Script from 'next/script';
 
 export default function Home() {
   return (
@@ -8,10 +8,7 @@ export default function Home() {
       <nav id="header" className="fixed w-full z-30 top-0 text-white">
         <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
           <div className="pl-4 flex items-center">
-            <a
-              className="toggleColour text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl"
-              href="#"
-            >
+            <a className="toggleColour text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl" href="#">
               Bank Sampah v.0.1.0
             </a>
           </div>
@@ -20,11 +17,7 @@ export default function Home() {
               id="nav-toggle"
               className="flex items-center p-1 text-pink-800 hover:text-gray-900 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
             >
-              <svg
-                className="fill-current h-6 w-6"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
+              <svg className="fill-current h-6 w-6" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                 <title>Menu</title>
                 <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
               </svg>
@@ -34,23 +27,14 @@ export default function Home() {
             className="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden mt-2 lg:mt-0 bg-white lg:bg-transparent text-black p-4 lg:p-0 z-20"
             id="nav-content"
           >
-            <ul
-              id="nav-subs"
-              className="list-reset lg:flex justify-end flex-1 items-center text-white"
-            >
+            <ul id="nav-subs" className="list-reset lg:flex justify-end flex-1 items-center text-white">
               <li className="mr-3">
-                <a
-                  className="inline-block  no-underline hover:font-semibold hover:scale-105 py-2 px-4"
-                  href="#features"
-                >
+                <a className="inline-block  no-underline hover:font-semibold hover:scale-105 py-2 px-4" href="#features">
                   Fitur
                 </a>
               </li>
               <li className="mr-3">
-                <a
-                  className="inline-block  no-underline hover:font-semibold hover:scale-105 py-2 px-4"
-                  href="#download"
-                >
+                <a className="inline-block  no-underline hover:font-semibold hover:scale-105 py-2 px-4" href="#download">
                   Unduh
                 </a>
               </li>
@@ -60,13 +44,7 @@ export default function Home() {
                 id="navAction"
                 className="mx-auto lg:mx-0 hover:underline align-middle bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-4 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out flex flex-row justify-center"
               >
-                <Image
-                  width={8}
-                  height={8}
-                  alt="Picture of the author"
-                  className="md:w-4/5 max-w-[30px] max-h-[30px] pr-2"
-                  src="github-mark.svg"
-                />
+                <Image width={8} height={8} alt="Picture of the author" className="md:w-4/5 max-w-[30px] max-h-[30px] pr-2" src="github-mark.svg" />
                 <p className="">GitHub</p>
               </button>
             </a>
@@ -79,13 +57,10 @@ export default function Home() {
         <div className="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
           {/* <!--Left Col--> */}
           <div className="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left">
-            <h1 className="my-4 text-5xl font-bold leading-tight">
-              Kumpulkan sampah, tukar Rupiah
-            </h1>
+            <h1 className="my-4 text-5xl font-bold leading-tight">Kumpulkan sampah, tukar Rupiah</h1>
             <p className="leading-normal text-2xl mb-8">
-              Dengan aplikasi Bank Sampah, jadilah bagian dari solusi untuk
-              lingkungan yang hijau. Dapatkan tunai melalui kontribusi anda,
-              peroleh manfaat dari sampah anda.
+              Dengan aplikasi Bank Sampah, jadilah bagian dari solusi untuk lingkungan yang hijau. Dapatkan tunai melalui kontribusi anda, peroleh
+              manfaat dari sampah anda.
             </p>
             <a href="bank_sampah_v0.1.1.apk">
               <button className="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
@@ -95,28 +70,14 @@ export default function Home() {
           </div>
           {/* <!--Right Col--> */}
           <div className="w-fit md:w-3/5 md:pr-24 py-6 text-center flex justify-end">
-            <Image
-              width={270}
-              height={600}
-              alt="Picture of the author"
-              className="md:w-4/5 max-w-[230px] max-h-[510px]"
-              src="/whatever.png"
-            />
+            <Image width={270} height={600} alt="Picture of the author" className="md:w-4/5 max-w-[230px] max-h-[510px]" src="/whatever.png" />
           </div>
         </div>
       </div>
       <div className="relative -mt-12 lg:-mt-24">
-        <svg
-          viewBox="0 0 1428 174"
-          version="1.1"
-          xmlns="http://www.w3.org/2000/svg"
-        >
+        <svg viewBox="0 0 1428 174" version="1.1" xmlns="http://www.w3.org/2000/svg">
           <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-            <g
-              transform="translate(-2.000000, 44.000000)"
-              fill="#FFFFFF"
-              fill-rule="nonzero"
-            >
+            <g transform="translate(-2.000000, 44.000000)" fill="#FFFFFF" fill-rule="nonzero">
               <path
                 d="M0,0 C90.7283404,0.927527913 147.912752,27.187927 291.910178,59.9119003 C387.908462,81.7278826 543.605069,89.334785 759,82.7326078 C469.336065,156.254352 216.336065,153.6679 0,74.9732496"
                 opacity="0.100000001"
@@ -131,11 +92,7 @@ export default function Home() {
                 opacity="0.200000003"
               ></path>
             </g>
-            <g
-              transform="translate(-4.000000, 76.000000)"
-              fill="#FFFFFF"
-              fill-rule="nonzero"
-            >
+            <g transform="translate(-4.000000, 76.000000)" fill="#FFFFFF" fill-rule="nonzero">
               <path d="M0.457,34.035 C57.086,53.198 98.208,65.809 123.822,71.865 C181.454,85.495 234.295,90.29 272.033,93.459 C311.355,96.759 396.635,95.801 461.025,91.663 C486.76,90.01 518.727,86.372 556.926,80.752 C595.747,74.596 622.372,70.008 636.799,66.991 C663.913,61.324 712.501,49.503 727.605,46.128 C780.47,34.317 818.839,22.532 856.324,15.904 C922.689,4.169 955.676,2.522 1011.185,0.432 C1060.705,1.477 1097.39,3.129 1121.236,5.387 C1161.703,9.219 1208.621,17.821 1235.4,22.304 C1285.855,30.748 1354.351,47.432 1440.886,72.354 L1441.191,104.352 L1.121,104.031 L0.457,34.035 Z"></path>
             </g>
           </g>
@@ -143,18 +100,14 @@ export default function Home() {
       </div>
       <section id="features" className="bg-white border-b py-8">
         <div className="container max-w-5xl mx-auto m-8">
-          <h2 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
-            Fitur
-          </h2>
+          <h2 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">Fitur</h2>
           <div className="w-full mb-4">
             <div className="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
           </div>
           {/* warga */}
           <div className="flex flex-wrap">
             <div className="w-5/6 sm:w-1/2 p-6">
-              <h3 className="text-3xl text-gray-800 font-bold leading-none mb-3">
-                Warga
-              </h3>
+              <h3 className="text-3xl text-gray-800 font-bold leading-none mb-3">Warga</h3>
               <ul className="text-black">
                 <li>Melihat saldo saat ini</li>
                 <li>Melihat jumlah sampah yang sudah di depositkan</li>
@@ -162,41 +115,21 @@ export default function Home() {
               </ul>
             </div>
             <div className="w-full sm:w-1/2 p-6">
-              <Image
-                width={8}
-                height={8}
-                alt="Picture of the author"
-                className="md:w-4/5 w-full max-h-[200px]"
-                src="warga.svg"
-              />
+              <Image width={8} height={8} alt="Picture of the author" className="md:w-4/5 w-full max-h-[200px]" src="warga.svg" />
             </div>
           </div>
           {/* staff */}
           <div className="flex flex-wrap flex-col-reverse sm:flex-row">
             <div className="w-full sm:w-1/2 p-6 mt-6">
-              <Image
-                width={8}
-                height={8}
-                alt="Picture of the author"
-                className="md:w-4/5 w-full max-h-[200px]"
-                src="staff.svg"
-              />
+              <Image width={8} height={8} alt="Picture of the author" className="md:w-4/5 w-full max-h-[200px]" src="staff.svg" />
             </div>
             <div className="w-full sm:w-1/2 p-6 mt-6">
               <div className="align-middle">
-                <h3 className="text-3xl text-gray-800 font-bold leading-none mb-3">
-                  Staff
-                </h3>
+                <h3 className="text-3xl text-gray-800 font-bold leading-none mb-3">Staff</h3>
                 <ul className="text-black">
                   <li>Mencatat deposit sampah warga</li>
-                  <li>
-                    Memodifikasi catatan deposit sampah warga dalam jangka waktu
-                    yang sudah ditentukan
-                  </li>
-                  <li>
-                    Melihat daftar transaksi yang telah dilakukan `deposit
-                    sampah` atau `tarik saldo`
-                  </li>
+                  <li>Memodifikasi catatan deposit sampah warga dalam jangka waktu yang sudah ditentukan</li>
+                  <li>Melihat daftar transaksi yang telah dilakukan `deposit sampah` atau `tarik saldo`</li>
                   <li>Melakukan penarikan saldo warga</li>
                 </ul>
               </div>
@@ -205,47 +138,26 @@ export default function Home() {
           {/* admin */}
           <div className="flex flex-wrap">
             <div className="w-5/6 sm:w-1/2 p-6">
-              n
-              <h3 className="text-3xl text-gray-800 font-bold leading-none mb-3">
-                Admin
-              </h3>
+              n<h3 className="text-3xl text-gray-800 font-bold leading-none mb-3">Admin</h3>
               <ul className="text-black">
                 <li>Melihat laporan bulanan berdasarkan desa yang dipilih</li>
-                <li>
-                  Mencetak laporan bulanan berdasarkan desa yang dipilih `pdf`
-                </li>
+                <li>Mencetak laporan bulanan berdasarkan desa yang dipilih `pdf`</li>
                 <li>Membuat pengguna baru</li>
                 <li>Memodifikasi pengguna lama</li>
-                <li>
-                  Mengetahui semua daftar pengguna `warga`, `staff` dan `admin`
-                </li>
+                <li>Mengetahui semua daftar pengguna `warga`, `staff` dan `admin`</li>
                 <li>Mengubah harga sampah organik dan an-organik per kg</li>
-                <li>
-                  Mengetahui riwayat perubahan harga sampah organik maupun
-                  an-organik
-                </li>
+                <li>Mengetahui riwayat perubahan harga sampah organik maupun an-organik</li>
               </ul>
             </div>
             <div className="w-full sm:w-1/2 p-6">
-              <Image
-                width={8}
-                height={8}
-                alt="Picture of the author"
-                className="md:w-4/5 w-full max-h-[200px]"
-                src="admin.svg"
-              />
+              <Image width={8} height={8} alt="Picture of the author" className="md:w-4/5 w-full max-h-[200px]" src="admin.svg" />
             </div>
           </div>
         </div>
       </section>
 
       {/* <!-- Change the colour #f8fafc to match the previous section colour --> */}
-      <svg
-        className="wave-top"
-        viewBox="0 0 1439 147"
-        version="1.1"
-        xmlns="http://www.w3.org/2000/svg"
-      >
+      <svg className="wave-top" viewBox="0 0 1439 147" version="1.1" xmlns="http://www.w3.org/2000/svg">
         <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
           <g transform="translate(-1.000000, -14.000000)" fill-rule="nonzero">
             <g className="wave" fill="#f8fafc">
@@ -271,18 +183,13 @@ export default function Home() {
         </g>
       </svg>
       <section className="container mx-auto text-center py-6 mb-12">
-        <h2
-          id="download"
-          className="w-full my-2 text-5xl font-bold leading-tight text-center text-white"
-        >
+        <h2 id="download" className="w-full my-2 text-5xl font-bold leading-tight text-center text-white">
           Tunggu apa lagi?
         </h2>
         <div className="w-full mb-4">
           <div className="h-1 mx-auto bg-white w-1/6 opacity-25 my-0 py-0 rounded-t"></div>
         </div>
-        <h3 className="my-4 text-3xl leading-tight">
-          Dapatkan aplikasinya sekarang!
-        </h3>
+        <h3 className="my-4 text-3xl leading-tight">Dapatkan aplikasinya sekarang!</h3>
         <a href="bank_sampah_v0.1.1.apk">
           <button className="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
             Unduh Sekarang!
@@ -294,12 +201,12 @@ export default function Home() {
         <div className="container mx-auto px-8 text-black flex justify-center">
           <p>© Developer Team | Bank Sampah 2023. All Right Reserved</p>
         </div>
-        <a
+        {/* <a
           href="https://www.freepik.com/free-photos-vectors/background"
           className="text-gray-500"
         >
           Background vector created by freepik - www.freepik.com
-        </a>
+        </a> */}
       </footer>
       {/* <!-- jQuery if you need it <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> --> */}
       <Script src="/js/home.js"></Script>
